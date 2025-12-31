a5a84fac2c316ed3f85f2ce0b1628bd8f038a7b0
Manit-Suthar <manitsuthar2007@gmail.com>
2025-12-31 11:17:05 +0530

    Add files via upload

---
+++ Problem109_maxDepthOfBinaryTress.java
@@ -0,0 +1,44 @@
+package p109;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+
+public class Problem109_maxDepthOfBinaryTress {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int depth = obj.maxDepth(root);
+        System.out.println("Max Depth of the Binary Tree: " + depth);
+    }
+}
---
+++ Problem110_invertABInaryTree.java
@@ -0,0 +1,54 @@
+package p110;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        {
+            if (root == null)
+                return null;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            invertTree(root.left);
+            invertTree(root.right);
+
+            return root;
+        }
+    }
+
+}
+
+public class Problem110_invertABInaryTree {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode invertedRoot = obj.invertTree(root);    
+    }
+}
