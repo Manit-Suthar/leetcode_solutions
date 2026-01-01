31276eae795cffd6b2a5d236afb48cbc53bf1422
Manit-Suthar <manitsuthar2007@gmail.com>
2026-01-01 23:35:26 +0530

    Add files via upload

---
+++ Problem116_pathSum.java
@@ -0,0 +1,48 @@
+package p116;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        int newTargetSum = targetSum - root.val;
+        return hasPathSum(root.left, newTargetSum) || hasPathSum(root.right, newTargetSum);
+    }
+}
+public class Problem116_pathSum {
+    public static void main(String[] args) {
+        Solution obj = new Solution();  
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+        int targetSum = 22;
+        boolean result = obj.hasPathSum(root, targetSum);
+        System.out.println(result);
+    }
+}
